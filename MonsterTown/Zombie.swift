1bb2b7e338c9762850db56d6d2d132ca966de968
Kevin Tran <kevin2005tran@gmail.com>
2021-05-10 08:55:32 -0700

    finished monstertown excercise

---
+++ MonsterTown/Zombie.swift
@@ -0,0 +1,19 @@
+//
+//  Zombie.swift
+//  MonsterTown
+//
+//  Created by Kevin Tran on 5/20/17.
+//  Copyright © 2017 com.example. All rights reserved.
+//
+
+import Foundation
+
+//class Zombie: Monster {
+//    var walksWithLimp = true
+//    var address = "123 central ave"
+//    
+//    final override func terrorizeTown() {
+//        town?.changePopulation(by: -10)
+//        super.terrorizeTown()
+//    }
+//}
